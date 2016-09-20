99abaf899375f95bd041ad77c0c4799f9680d054
Sepehr Mohaimenianpour <voidminded@gmail.com>
2016-09-19 20:50:15 -0700

    World is kinda ready now

--- basic.cc
+++ basic.cc
@@ -13,29 +13,59 @@ typedef struct
 {
     ModelPosition* position;
     ModelRanger* ranger;
-    radians_t closest_bearing;
-    meters_t closest_range;
-    radians_t closest_heading_error;
-
+    ModelRanger* laser;
+    ModelFiducial* fiducial;
 } robot_t;
-
-
-static ModelPosition* position(NULL);
 
 /** this is added as a callback to a ranger model, and is called
     whenever the model is updated by Stage.
 */
 int RangerUpdate( ModelRanger* mod, robot_t* robot)
 {
-    const std::vector<ModelRanger::Sensor>& sensors = robot->ranger->GetSensors();
+    const std::vector<ModelRanger::Sensor>& sensors = mod->GetSensors();
 
     // ( inspect the ranger data and decide what to do )
 
-    // output something to prove it is working
     char toSay[9];
     sprintf( toSay, "%d", robot->position->GetId());
     robot->ranger->Say( toSay); // GUI window
-    robot->position->SetSpeed( 0.4, 0, 0.1 );  // output a speed command (X, Y, Z)
+    int rot = 0;
+    for( int i = 0; i < 4; i++)
+        if( sensors[i].ranges[0] < 5)
+            rot = -1;
+    for( int i = 4; i < 8; i++)
+        if( sensors[i].ranges[0] < 5)
+            rot = 1;
+    robot->position->SetSpeed( 0.4, 0, rot*0.3 );  // output a speed command (X, Y, Z)
+
+
+    return 0;
+}
+
+int LaserUpdate( ModelRanger* mod, robot_t* robot)
+{
+    const std::vector<meters_t>& scan = robot->laser->GetSensors()[0].ranges;
+    int sample_count = scan.size();
+    if( sample_count < 1 )
+        return 0;
+    std::cout << mod->GetId() << " -- ";
+    for (uint32_t i = 0; i < sample_count; i++){
+        std::cout  << i << " -> " << scan[i] << " | ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
+
+int FiducialUpdate( ModelFiducial* fid, robot_t* robot)
+{
+    // find the closest teammate
+    unsigned int n=0;
+    ModelFiducial::Fiducial* fd = fid->GetFiducials(&n);
+    if(n > 0){
+        for(int i=0;i<n;i++){
+            std::cout << fd[i].id << std::endl;
+        }
+    }
     return 0;
 }
 
@@ -47,13 +77,22 @@ extern "C" int Init( Model* mod )
     robot->position = (ModelPosition*)mod;
 
     // subscribe to the ranger, which we use for navigating
-    robot->ranger = (ModelRanger*)mod->GetUnusedModelOfType( "ranger" );
+    robot->ranger = (ModelRanger*)mod->GetChild( "ranger:0" );
     assert( robot->ranger );
-
+    robot->laser = (ModelRanger*)mod->GetChild( "ranger:1" );
+    assert( robot->laser );
+    robot->fiducial = (ModelFiducial*)mod->GetUnusedModelOfType( "fiducial" ) ;
+    assert( robot->fiducial );
     // ask Stage to call into our ranger update function
     robot->ranger->AddCallback( Model::CB_UPDATE, (model_callback_t)RangerUpdate, robot );
+    robot->laser->AddCallback( Model::CB_UPDATE, (model_callback_t)LaserUpdate, robot );
+    robot->fiducial->AddCallback( Model::CB_UPDATE, (model_callback_t)FiducialUpdate, robot );
+
     robot->ranger->Subscribe();
     robot->position->Subscribe();
+    robot->laser->Subscribe();
+    robot->fiducial->Subscribe();
+
     return 0; //ok
 }
 
