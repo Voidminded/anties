60552ec47a5b3b057e229d682a2764fa8dc4a21d
Sepehr Mohaimenianpour <voidminded@gmail.com>
2016-09-19 22:39:19 -0700

    World is kinda ready now

--- basic.cc
+++ basic.cc
@@ -7,7 +7,18 @@
 /////////////////////////////////
 
 #include "stage.hh"
+#include "map"
+#include "math.h"
 using namespace Stg;
+
+enum RobotState
+{
+    UNKNOWN  = 0,
+    SEARCHING = 1,
+    HOMING = 2
+};
+
+#define VEL = 0.9;
 
 typedef struct
 {
@@ -15,7 +26,19 @@ typedef struct
     ModelRanger* ranger;
     ModelRanger* laser;
     ModelFiducial* fiducial;
+    enum RobotState state;
+    bool foundDist;
+    bool avoiding;
+    Pose target;
 } robot_t;
+
+typedef struct
+{
+    double w;
+    double ph = 0;
+}cell;
+
+std::map <std::pair< int, int>, cell> map;
 
 /** this is added as a callback to a ranger model, and is called
     whenever the model is updated by Stage.
@@ -29,16 +52,22 @@ int RangerUpdate( ModelRanger* mod, robot_t* robot)
     char toSay[9];
     sprintf( toSay, "%d", robot->position->GetId());
     robot->ranger->Say( toSay); // GUI window
-    int rot = 0;
-    for( int i = 0; i < 4; i++)
-        if( sensors[i].ranges[0] < 5)
-            rot = -1;
-    for( int i = 4; i < 8; i++)
-        if( sensors[i].ranges[0] < 5)
-            rot = 1;
-    robot->position->SetSpeed( 0.4, 0, rot*0.3 );  // output a speed command (X, Y, Z)
+//    for( int i = 0; i < robot->position->waypoints.size(); i++)
+//    std::cout << robot->position->GetGlobalPose().x << robot->position->GetGlobalPose().y << " " << robot->position->GetGlobalPose().a << std::endl;
 
 
+    // map update thingy
+    for( int i = -20; i < 20; i++)
+        for( int j = -20; j < 20; j++)
+            if(map[std::pair<int, int>(i, j)].ph > 0)
+                map[std::pair<int, int>(i, j)].ph -= 0.001;
+    map[std::pair<int, int>(round( robot->position->GetGlobalPose().x), round( robot->position->GetGlobalPose().y))].ph += 3;
+    map[std::pair<int, int>(round( robot->position->GetGlobalPose().x), round( robot->position->GetGlobalPose().y))].w = robot->position->GetGlobalPose().a;
+
+//    for( int i = -20; i < 20; i++)
+//        for( int j = -20; j < 20; j++)
+//            std::cout << map[std::pair<int, int>(i, j)].w;
+//    std::cout << std::endl;
     return 0;
 }
 
@@ -48,11 +77,15 @@ int LaserUpdate( ModelRanger* mod, robot_t* robot)
     int sample_count = scan.size();
     if( sample_count < 1 )
         return 0;
-    std::cout << mod->GetId() << " -- ";
-    for (uint32_t i = 0; i < sample_count; i++){
-        std::cout  << i << " -> " << scan[i] << " | ";
-    }
-    std::cout << std::endl;
+    int rot = 0;
+    for( int i = 0; i < 90; i++)
+        if( scan[0] < 3)
+            rot = -1;
+    for( int i = 90; i < 180; i++)
+        if( scan[0] < 3)
+            rot = 1;
+    robot->position->SetSpeed( 0.9, 0, rot*0.3 );
+    robot->avoiding = rot;
     return 0;
 }
 
@@ -61,9 +94,21 @@ int FiducialUpdate( ModelFiducial* fid, robot_t* robot)
     // find the closest teammate
     unsigned int n=0;
     ModelFiducial::Fiducial* fd = fid->GetFiducials(&n);
+    robot->foundDist = false;
     if(n > 0){
         for(int i=0;i<n;i++){
-            std::cout << fd[i].id << std::endl;
+            if( fd[i].id == 3 && robot->state == RobotState::SEARCHING)
+            {
+                robot->foundDist = true;
+                robot->target.x = fd[i].pose.x;
+                robot->target.y = fd[i].pose.y;
+            }
+            else if( fd[i].id == 33 && robot->state == RobotState::HOMING)
+            {
+                robot->foundDist = true;
+                robot->target.x = fd[i].pose.x;
+                robot->target.y = fd[i].pose.y;
+            }
         }
     }
     return 0;
@@ -93,6 +138,17 @@ extern "C" int Init( Model* mod )
     robot->laser->Subscribe();
     robot->fiducial->Subscribe();
 
+    //Robot initialization
+    robot->state = RobotState::SEARCHING;
+    robot->foundDist = false;
+    robot->avoiding = false;
+
+    for( int i = -20; i < 20; i++)
+        for( int j = -20; j < 20; j++)
+        {
+            map[std::pair<int, int>(i, j)].w = 0;
+            map[std::pair<int, int>(i, j)].ph = 0;
+        }
     return 0; //ok
 }
 
